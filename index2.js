3cd295f63a2d8dcdee9e13d1512bf7461af2e121
XxUnderyourbedxX <116187837+XxUnderyourbedxX@users.noreply.github.com>
2023-03-22 10:43:22 -0700

    hj

--- index2.js
+++ index2.js
@@ -52,6 +52,55 @@ function myFunction(){
   let x = document.getElementById()
 }
 
+const draggbless = document.querySelectorAll(".shallow-draggable")
+const containerss = document.querySelectorAll(".draggable_container")
+
+draggbless.forEach((draggble) => {
+  //for start dragging costing opacity
+  draggble.addEventListener("dragstart", () => {
+    draggble.classList.add("dragging")
+  })
+
+  //for end the dragging opacity costing
+  draggble.addEventListener("dragend", () => {
+    draggble.classList.remove("dragging")
+  })
+})
+//something
+containerss.forEach((container) => {
+  container.addEventListener("dragover", function (e) {
+    e.preventDefault()
+    const afterElement = dragAfterElement(container, e.clientY)
+    const dragging = document.querySelector(".dragging")
+    if (afterElement == null) {
+      container.appendChild(dragging)
+    } else {
+      container.insertBefore(dragging, afterElement)
+    }
+  })
+})
+
+function dragAfterElement(container, y) {
+  const draggbleElements = [...container.querySelectorAll(".shallow-draggable:not(.dragging)")]
+
+  return draggbleElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element
+}
+
+function myFunction(){
+  let x = document.getElementById()
+}
+
 /*const multiDimensionalArray = [
   [0,2,1,0,0],
   [2,0,0,3,0],
